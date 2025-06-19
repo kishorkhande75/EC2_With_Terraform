241eea5c020700a7328d7a7342f7bd8258d2f85b
kishorkhande75 <98527960+kishorkhande75@users.noreply.github.com>
2025-06-19 13:10:21 +0530

    Update provider.tf
    access and secret key variables

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 ## define your cloud service provide
 provider "aws" {
   region = "us-east-1" # Change this to your desired AWS region
+  access_key = var.access.key
+  secret_key = var.secret.key
 }
